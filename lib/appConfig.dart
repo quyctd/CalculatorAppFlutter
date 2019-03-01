5ab74e1e156445628d5064bf4487a8475fffc047
quyctd <quy.dc98@gmail.com>
2019-03-02 00:57:31 +0700

    Added logic

--- lib/appConfig.dart
+++ lib/appConfig.dart
@@ -1,3 +1,9 @@
 final double CAL_BUTTON_PADDING = 20.0;
 final double CAL_TEXT_SIZE = 25.0;
 final double RESULT_TEXT_SIZE = 50.0;
+
+var displayRes = "0";
+var result = "0";
+var num1 = 0;
+var num2 = 0;
+var operand = "";
--- lib/main.dart
+++ lib/main.dart
@@ -31,20 +31,52 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  buttonPressed(String value) {
+    if (value == "AC") {
+      result = "0";
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+    }
+    else if (value == "+" || value == "-" || value == "x" || value == "/") {
+        num1 = int.parse(displayRes);
+        operand = value;
+        result = "";
+    }
+    else if (value == "=") {
+      num2 = int.parse(displayRes);
+      if (operand == "+") result = (num1 + num2).toString();
+      if (operand == "-") result = (num1 - num2).toString();
+      if (operand == "/") result = (num1 / num2).toString();
+      if (operand == "x") result = (num1 * num2).toString();
+
+    }
+    else {
+      result = result + value;
+    }
+
+    setState(() {
+      if (operand == "/" && value == "=")
+        displayRes = double.parse(result).toStringAsFixed(3);
+      else displayRes = int.parse(result).toString();
+
+      if (value == "=") {
+        num1 = 0;
+        num2 = 0;
+        operand = "";
+      }
+    });
+  }
+
   Widget myButton(var value) {
     return Expanded(
         child: OutlineButton(
-          padding: EdgeInsets.all(CAL_BUTTON_PADDING),
-          child: Text(
-            value,
-            style: TextStyle(
-              fontSize: CAL_TEXT_SIZE,
-              fontWeight: FontWeight.bold
-            )
-          ),
-          onPressed: () => {},
-        )
-    );
+      padding: EdgeInsets.all(CAL_BUTTON_PADDING),
+      child: Text(value,
+          style:
+              TextStyle(fontSize: CAL_TEXT_SIZE, fontWeight: FontWeight.bold)),
+      onPressed: () => buttonPressed(value),
+    ));
   }
 
   @override
@@ -52,29 +84,22 @@ class _MyHomePageState extends State<MyHomePage> {
     // This method is rerun every time setState is called, for instance as done
 
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Container(
-        child: Column( children: <Widget>[
-
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Container(
+            child: Column(children: <Widget>[
           //result go here
           Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.symmetric(
-              vertical: 25.0,
-              horizontal: 15.0
-            ),
-            child: Text(
-              "0",
-              style: TextStyle(
-                fontSize: RESULT_TEXT_SIZE,
-                fontWeight: FontWeight.bold
-              ),
-            )
-          ),
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 15.0),
+              child: Text(
+                displayRes,
+                style: TextStyle(
+                    fontSize: RESULT_TEXT_SIZE, fontWeight: FontWeight.bold),
+              )),
 
           Expanded(child: Divider()),
 
@@ -82,48 +107,38 @@ class _MyHomePageState extends State<MyHomePage> {
           Column(
             children: <Widget>[
               //Row 1: number : 1 2 3
-              Row (children:[
-                  myButton("AC"),
-                  myButton(""),
-                  myButton("%"),
-                  myButton("/")
-
+              Row(children: [
+                myButton("AC"),
+                myButton(""),
+                myButton(""),
+                myButton("/")
               ]),
 
               //Row 2
-              Row (children:[
+              Row(children: [
                 myButton("7"),
                 myButton("8"),
                 myButton("9"),
                 myButton("x")
               ]),
 
-              Row (children:[
+              Row(children: [
                 myButton("4"),
                 myButton("5"),
                 myButton("6"),
                 myButton("-")
               ]),
 
-              Row (children:[
+              Row(children: [
                 myButton("1"),
                 myButton("2"),
                 myButton("3"),
                 myButton("+")
-
               ]),
 
-              Row (children:[
-                myButton("0"),
-                myButton("=")
-
-              ]),
+              Row(children: [myButton("0"), myButton("=")]),
             ],
           )
-
-          ]
-        )
-      )
-    );
+        ])));
   }
 }
